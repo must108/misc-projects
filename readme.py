19ce710de7d37ce31a6de97dbf0ad75c1ac153f9
must108 <136145181+must108@users.noreply.github.com>
2024-04-24 02:15:33 -0400

    push

---
+++ readme.py
@@ -0,0 +1,15 @@
+class me:
+    name = "mustaeen ahmed"
+    profession = "software engineer"
+    major = "computer science"
+    school = "university of central florida"
+    experience = {
+        "software developer" : "association for computing machinery"
+    }
+    passions = [
+        "machine learning",
+        "data analytics",
+        "data visualization",
+        "web development",
+        "application development"
+    ]
