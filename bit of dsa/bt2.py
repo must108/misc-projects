9188d0d6704db24596841e613fa41367022acc99
must108 <mustaeen18@gmail.com>
2025-01-14 00:37:03 -0500

    finished binary tree review

--- bit of dsa/bt2.py
+++ bit of dsa/bt2.py
@@ -1,85 +1,93 @@
 
+# treenode
+# preorder
+# inorder
+# postorder
+# iterative preorder
+# level order
+# search
+# bst search
+
 class TreeNode:
-    def __init__(self, val, left=None, right=None):
+    def __init__(self, val, left = None, right = None):
         self.val = val
         self.left = left
         self.right = right
 
-A = TreeNode(1)
-B = TreeNode(2)
-C = TreeNode(3)
-D = TreeNode(4)
-E = TreeNode(5)
-F = TreeNode(10)
+# regular binary tree
+A, B, C, D, E, F, G = [
+    TreeNode(1), TreeNode(3), TreeNode(17), TreeNode(14), TreeNode(5), 
+    TreeNode(2), TreeNode(15)
+]
 
-A.left = B
-A.right = C
-B.left = D
-B.right = E
-C.left = F
+A.left, A.right = B, C
+B.left, B.right = D, E
+C.left, C.right = F, G
 
-### RECURSIVE ###
-
+# root => left => right
 def preorder(node):
     if not node:
         return None
-    
+
     print(node.val, end=" ")
-    preorder(node.left)
-    preorder(node.right)
+    if node.left: preorder(node.left)
+    if node.right: preorder(node.right)
 
 preorder(A)
 print("\n")
 
+# left => root => right
 def inorder(node):
     if not node:
         return None
 
-    inorder(node.left)
+    if node.left: inorder(node.left)
     print(node.val, end=" ")
-    inorder(node.right)
+    if node.right: inorder(node.right)
 
 inorder(A)
 print("\n")
 
+# left => right => root
 def postorder(node):
     if not node:
         return None
-    
-    postorder(node.left)
-    postorder(node.right)
+
+    if node.left: postorder(node.left)
+    if node.right: postorder(node.right)
     print(node.val, end=" ")
 
 postorder(A)
 print("\n")
 
-### ITERATIVE ###
-
+# left => right => root (but iterative)
 def preorder_iter(node):
-    st = [node]
-
-    while st:
-        a = st.pop()
+    s = [node]
+    
+    while s:
+        a = s.pop()
         print(a.val, end=" ")
-        if a.right: st.append(a.right)
-        if a.left: st.append(a.left)
+        if a.right: s.append(a.right)
+        if a.left: s.append(a.left)
 
 preorder_iter(A)
 print("\n")
 
-def levelorder(node):
+# level order. level by level. bfs.
+def bfs(node):
     q = [node]
 
     while q:
         a = q.pop(0)
         print(a.val, end=" ")
+
         if a.left: q.append(a.left)
         if a.right: q.append(a.right)
 
-
-levelorder(A)
+bfs(A)
 print("\n")
 
+# search. some real shit.
 def search(node, val):
     if not node:
         return False
@@ -89,42 +97,45 @@ def search(node, val):
 
     return search(node.left, val) or search(node.right, val)
 
-def check_searched_val(node, val):
+def find_val_bt(node, val):
     if search(node, val):
         print("Found:", val)
     else:
         print("Not found:", val)
 
-check_searched_val(A, 2)
-check_searched_val(A, 15)
+find_val_bt(A, 5)
+find_val_bt(A, 18)
 
-A2 = TreeNode(8)
-B2 = TreeNode(5)
-C2 = TreeNode(15)
-D2 = TreeNode(3)
-E2 = TreeNode(7)
-F2 = TreeNode(10)
-G2 = TreeNode(17)
+### BINARY SEARCH TREE
+
+A2, B2, C2, D2, E2, F2, G2 = [
+    TreeNode(8), TreeNode(5), TreeNode(15), TreeNode(3), TreeNode(7),
+    TreeNode(10), TreeNode(17)
+]
 
 A2.left, A2.right = B2, C2
 B2.left, B2.right = D2, E2
 C2.left, C2.right = F2, G2
 
+bfs(A2)
+print("\n")
+
+# optimized, given the nature of a bst
 def search_bst(node, val):
     if not node:
         return False
 
-    if (val == node.val):
+    if val == node.val:
         return True
 
     if val < node.val: search_bst(node.left, val)
     else: search_bst(node.right, val)
 
-def check_searched_bst(node, val):
+def find_val_bst(node, val):
     if search_bst(node, val):
         print("Found:", val)
     else:
         print("Not found:", val)
 
-check_searched_bst(A2, 8)
-check_searched_bst(A2, 25)
+find_val_bst(A2, 8)
+find_val_bst(A2, 18)
